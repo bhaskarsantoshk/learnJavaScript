6bea2030db2bac590adad6ca35afcaa82c457022
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2019-05-22 12:30:59 +0530

    typeconversions.js

---
+++ typeconversions.js
@@ -0,0 +1,15 @@
+let a= "" + 1 + 0
+let b = "" - 1 + 0
+let c= true + false
+let d= 6 / "3"
+let e="2" * "3"
+let f= 4 + 5 + "px"
+let g="$" + 4 + 5
+let h="4" - 2
+let i="4px" - 2
+let j=7 / 0
+let k="  -9  " + 5
+let l="  -9  " - 5
+let m=null + 1
+let n= undefined + 1
+alert(a+" "+b+" "+c+" "+d+" "+e+" "+f+" "+g+" "+h+" "+i+" "+j+" "+k+" "+l+" "+m+" "+n);
